9bbf39a009afdc04fd838a0a2eb059eb81e8296a
chenchao04 <hendiwuhuan@qq.com>
2019-06-25 19:06:40 +0800

    auto find phpize path

--- build.sh
+++ build.sh
@@ -2,6 +2,25 @@
 dirname $0
 cd `dirname $0`
 make clean
-/usr/local/php/bin/phpize
-./configure --with-php-config=/usr/local/php/bin/php-config --enable-debug
-make && make install && service php-fpm restart
+
+#find out phpize
+if $(command -v phpize 2>/dev/null);then
+    echo "find phpize ...";
+else
+    echo "can not find phpize ...";
+    echo "begin to search phpize ...";
+    findResults=`find / -name  phpize|head -n 1`
+    if $(command -v ${findResults} 2>/dev/null);then
+        echo "find phpize ...";
+    else
+	echo "can not find phpize";
+        exit 1
+    fi
+fi
+
+#find out php-config
+PHP_CONFIG=`which php-config|head -n 1`
+
+
+./configure --with-php-config=${PHP_CONFIG} --enable-debug
+make && make install && killall php-fpm && service php-fpm restart
